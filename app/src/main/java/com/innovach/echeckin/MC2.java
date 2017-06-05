04930749d17b49092c0b558decaa598120f6410f
viv-iam <vivian.opondoh@gmail.com>
2017-06-05 17:22:44 +0300

    Create MC2 activity

--- app/src/main/java/com/innovach/echeckin/MC2.java
+++ app/src/main/java/com/innovach/echeckin/MC2.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 public class MC2 extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedIstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mc2);
     }
