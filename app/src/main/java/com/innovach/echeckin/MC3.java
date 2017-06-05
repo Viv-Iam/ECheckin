e4d8abeb9c4026c857e58c96dbda4f61b74393ff
viv-iam <vivian.opondoh@gmail.com>
2017-06-05 17:31:15 +0300

    Create MC3 activity

--- app/src/main/java/com/innovach/echeckin/MC3.java
+++ app/src/main/java/com/innovach/echeckin/MC3.java
@@ -8,6 +8,6 @@ public class MC3 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_mc3);
+        setContentView(R.layout.activity_mc);
     }
 }
