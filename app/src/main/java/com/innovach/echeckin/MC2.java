9e58b19aa825915ec8adb58868bc7dbe67f4e0d6
viv-iam <vivian.opondoh@gmail.com>
2017-06-05 17:17:39 +0300

    Add Button views, Add intent upon click of these buttons *Add mc2, mc3, mpft4 and mppt button views in the activity_main.xml *Add intents to move from MainActivity.java to other select activities upon click of the for different buttons

---
+++ app/src/main/java/com/innovach/echeckin/MC2.java
@@ -0,0 +1,13 @@
+package com.innovach.echeckin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MC2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mc2);
+    }
+}
--- app/src/main/java/com/innovach/echeckin/MainActivity.java
+++ app/src/main/java/com/innovach/echeckin/MainActivity.java
@@ -1,13 +1,47 @@
 package com.innovach.echeckin;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @Bind(R.id.mc2button) Button mMc2Button;
+    @Bind(R.id.mc3button) Button mMc3Button;
+    @Bind(R.id.mpft4button) Button mMpft4Button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mMc2Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MC2.class);
+                startActivity(intent);
+            }
+        });
+
+        mMc3Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MC3.class);
+                startActivity(intent);
+            }
+        });
+
+        mMpft4Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MPFT4.class);
+                startActivity(intent);
+            }
+        });
     }
 }
