649ec00ca274aa3593378d4242c9659ba336ff9b
viv-iam <vivian.opondoh@gmail.com>
2017-06-05 17:31:59 +0300

    Create MPFT4 activity

--- app/src/main/java/com/innovach/echeckin/MC3.java
+++ app/src/main/java/com/innovach/echeckin/MC3.java
@@ -8,6 +8,6 @@ public class MC3 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_mc);
+        setContentView(R.layout.activity_mc3);
     }
 }
