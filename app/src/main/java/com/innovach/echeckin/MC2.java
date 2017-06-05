4e9aa6cf15d8c4406c012877a677a22db1fb9771
viv-iam <vivian.opondoh@gmail.com>
2017-06-05 17:24:18 +0300

    Define style NoActionTheme in styles.xml file
    *Apply NoActionTheme to the MainActivity

--- app/src/main/java/com/innovach/echeckin/MC2.java
+++ app/src/main/java/com/innovach/echeckin/MC2.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 public class MC2 extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedIstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mc2);
     }
