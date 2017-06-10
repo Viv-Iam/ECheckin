cdcc616176ccfefdbcd66408292c31b049b76969
viv-iam <vivian.opondoh@gmail.com>
2017-06-10 11:30:47 +0300

    Override built oCreateViewHolder in RecyclerViewHolder.View holder method

--- app/src/main/java/com/innovach/echeckin/adapter/NameListAdapter.java
+++ app/src/main/java/com/innovach/echeckin/adapter/NameListAdapter.java
@@ -1,8 +1,20 @@
 package com.innovach.echeckin.adapter;
 
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
+import com.innovach.echeckin.R;
+
+import java.util.List;
+import java.util.jar.Attributes;
+
+import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class NameListAdapter extends RecyclerView.Adapter<NameListAdapter.NameViewHolder> {
@@ -13,8 +25,19 @@ public class NameListAdapter extends RecyclerView.Adapter<NameListAdapter.NameVi
         mContext = context;
         mNames = names;
     }
+
+    @Override
+    public NameListAdapter.NameViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.name_list_item, parent, false);
+        NameViewHolder viewHolder = new NameViewHolder(view);
+        return viewHolder;
+    }
+
+
+
+
     public class NameViewHolder extends RecyclerView.ViewHolder {
-        @Bind(R.id.personNameTextView)  TextView mPersonNameTextView;
+        @Bind(R.id.personNameTextView) TextView mPersonNameTextView;
         @Bind(R.id.checkinButton) Button mCheckinButton;
         private Context mContext;
 
