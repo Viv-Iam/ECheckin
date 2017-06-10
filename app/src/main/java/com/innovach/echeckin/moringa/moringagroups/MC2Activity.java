8282b8a0d5c25505e06b0b8db7d25db1eedcc3ec
viv-iam <vivian.opondoh@gmail.com>
2017-06-10 07:19:35 +0300

    Refactor *Set file structure to have package moringa and create MC2, MC3, MPFT and MPPT activities in the package

---
+++ app/src/main/java/com/innovach/echeckin/moringa/moringagroups/MC2Activity.java
@@ -0,0 +1,15 @@
+package com.innovach.echeckin.moringa.moringagroups;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.innovach.echeckin.R;
+
+public class MC2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mc2);
+    }
+}
---
+++ app/src/main/java/com/innovach/echeckin/moringa/moringagroups/MPPTActivity.java
@@ -0,0 +1,15 @@
+package com.innovach.echeckin.moringa.moringagroups;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.innovach.echeckin.R;
+
+public class MPPTActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mppt);
+    }
+}
