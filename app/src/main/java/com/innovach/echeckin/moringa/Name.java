73e815c1ec8fed8f31c82cfe3862548ec2e59de5
viv-iam <vivian.opondoh@gmail.com>
2017-06-11 10:38:52 +0300

    Remove m prefix from member variables in Name.java model Double-check that public getter methods exist for each variable and that each is a valid JSON type.

--- app/src/main/java/com/innovach/echeckin/moringa/Name.java
+++ app/src/main/java/com/innovach/echeckin/moringa/Name.java
@@ -1,37 +1,38 @@
 package com.innovach.echeckin.moringa;
 
-import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by vivian on 10/06/17.
  */
 
 public class Name {
-    private String mName;
-    private boolean mCheckin;
+    String name;
+    boolean checkin;
+
+    public Name() {}
 
     public Name(String name, boolean checkin) {
-        mName = name;
-        mCheckin = checkin;
+        this.name = name;
+        this.checkin = checkin;
     }
 
     public String getName() {
-        return mName;
+        return name;
     }
 
     public boolean isCheckedin() {
-        return mCheckin;
+        return checkin;
     }
 
-    private static int lastContactId = 0;
-
-    public static ArrayList<Name> createContactsList(int numContacts) {
-        ArrayList<Name> contacts = new ArrayList<Name>();
-
-        for (int i = 1; i <= numContacts; i++) {
-            contacts.add(new Name("Person " + ++lastContactId, i <= numContacts / 2));
-        }
-
-        return contacts;
+//    private static int lastContactId = 0;
+//
+//    public static ArrayList<Name> createContactsList(int numContacts) {
+//        ArrayList<Name> contacts = new ArrayList<Name>();
+//
+//        for (int i = 1; i <= numContacts; i++) {
+//            contacts.add(new Name("Person " + ++lastContactId, i <= numContacts / 2));
+//        }
+//
+//        return contacts;
     }
-}
