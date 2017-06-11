d09a1a079a9be6bf966add54c129b4fba89f5e79
viv-iam <vivian.opondoh@gmail.com>
2017-06-11 07:37:10 +0300

    Add on click listener to AdminActivity.java enable save names to Firebase once enter name in EditText and click button

--- app/src/main/java/com/innovach/echeckin/moringa/AdminMainActivity.java
+++ app/src/main/java/com/innovach/echeckin/moringa/AdminMainActivity.java
@@ -7,32 +7,46 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.innovach.echeckin.Constants;
 import com.innovach.echeckin.R;
 import com.innovach.echeckin.moringa.moringagroups.MC2Activity;
+import com.innovach.echeckin.moringa.moringagroups.MC3Activity;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class AdminMainActivity extends AppCompatActivity {
+import static android.R.attr.name;
+
+public class AdminMainActivity extends AppCompatActivity implements View.OnClickListener {
+    private DatabaseReference mAddedNameReference;
     @Bind(R.id.nameEditText) EditText mNameEditText;
     @Bind(R.id.mc2NameButton) Button mMc2NameButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        mAddedNameReference = FirebaseDatabase.getInstance().getReference().child(Constants.FIREBASE_CHILD_ADDED_NAME);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_main);
         ButterKnife.bind(this);
-        mMc2NameButton.setOnClickListener(new View.OnClickListener() {
-            @Override
+        mMc2NameButton.setOnClickListener(this);
+    }
+
+    @Override
             public void onClick(View v) {
-                String name = mNameEditText.getText().toString();
-                Intent intent = new Intent(AdminMainActivity.this, MC2Activity.class);
-                intent.putExtra("location", name);
-                startActivity(intent);
+                if(v == mMc2NameButton) {
+                    String name = mNameEditText.getText().toString();
+                    saveNameToFirebase(name);
+                    Intent intent = new Intent(AdminMainActivity.this, MC2Activity.class);
+                    intent.putExtra("name", name);
+                    startActivity(intent);
+                }
 
             }
-        });
-
-
-    }
+            public void saveNameToFirebase(String name) {
+            mAddedNameReference.setValue(name);
+        }
 }
