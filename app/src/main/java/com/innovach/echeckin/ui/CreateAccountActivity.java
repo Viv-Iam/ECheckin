f36b5b0cb5980614ea99d99755cb0e17bbe43805
viv-iam <vivian.opondoh@gmail.com>
2017-06-12 19:02:01 +0300

    Bind all elements from activity_create_account.xml using ButterKnife

--- app/src/main/java/com/innovach/echeckin/ui/CreateAccountActivity.java
+++ app/src/main/java/com/innovach/echeckin/ui/CreateAccountActivity.java
@@ -3,11 +3,22 @@ package com.innovach.echeckin.ui;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.innovach.echeckin.R;
 
+import butterknife.Bind;
+
 public class CreateAccountActivity extends AppCompatActivity implements View.OnClickListener {
 
+    @Bind(R.id.createUserButton) Button mCreateUserButton;
+    @Bind(R.id.nameEditText) EditText mNameEditText;
+    @Bind(R.id.emailEditText) EditText mEmailEditText;
+    @Bind(R.id.passwordEditText) EditText mPasswordEditText;
+    @Bind(R.id.confirmPasswordEditText) EditText mConfirmPasswordEditText;
+    @Bind(R.id.loginTextView) TextView mLoginTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
