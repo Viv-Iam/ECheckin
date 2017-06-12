e4901c3668b41a13d29e9cf4e2b713c847004480
viv-iam <vivian.opondoh@gmail.com>
2017-06-12 20:46:22 +0300

    *Call built-in Firebase method createUserWithEmailAndPassword() *Add createNewUser() to fetch contents of registration form

--- app/src/main/java/com/innovach/echeckin/ui/CreateAccountActivity.java
+++ app/src/main/java/com/innovach/echeckin/ui/CreateAccountActivity.java
@@ -1,13 +1,19 @@
 package com.innovach.echeckin.ui;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.innovach.echeckin.R;
 
@@ -46,5 +52,24 @@ public class CreateAccountActivity extends AppCompatActivity implements View.OnC
         if (view == mCreateUserButton) {
             createNewUser();
         }
+    }
+    private void createNewUser() {
+        final String name = mNameEditText.getText().toString().trim();
+        final String email = mEmailEditText.getText().toString().trim();
+        String password = mPasswordEditText.getText().toString().trim();
+        String confirmPassword = mConfirmPasswordEditText.getText().toString().trim();
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "Authentication successful");
+                        } else {
+                            Toast.makeText(CreateAccountActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
