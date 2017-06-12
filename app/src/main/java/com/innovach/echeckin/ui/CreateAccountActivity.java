f4e2951877ee7611144f53038f00c0a980496181
viv-iam <vivian.opondoh@gmail.com>
2017-06-12 19:00:03 +0300

    Implement View.OnClickListener in CreateAccountActivity

--- app/src/main/java/com/innovach/echeckin/ui/CreateAccountActivity.java
+++ app/src/main/java/com/innovach/echeckin/ui/CreateAccountActivity.java
@@ -2,10 +2,12 @@ package com.innovach.echeckin.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.innovach.echeckin.R;
 
-public class CreateAccountActivity extends AppCompatActivity {
+public class CreateAccountActivity extends AppCompatActivity implements View.OnClickListener {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/innovach/echeckin/ui/LoginActivity.java
+++ app/src/main/java/com/innovach/echeckin/ui/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.innovach.echeckin.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -20,5 +21,14 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
         ButterKnife.bind(this);
         mRegisterTextView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view == mRegisterTextView) {
+            Intent intent = new Intent(LoginActivity.this, CreateAccountActivity.class);
+            startActivity(intent);
+            finish();
+        }
     }
 }
