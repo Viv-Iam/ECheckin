3341aaee28b60e48c8e13b7900fbf5b72cb1176f
viv-iam <vivian.opondoh@gmail.com>
2017-06-12 18:43:05 +0300

    Bind registerTextView in LoginActivity

--- app/src/main/java/com/innovach/echeckin/ui/LoginActivity.java
+++ app/src/main/java/com/innovach/echeckin/ui/LoginActivity.java
@@ -3,10 +3,14 @@ package com.innovach.echeckin.ui;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import com.innovach.echeckin.R;
 
+import butterknife.Bind;
+
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+    @Bind(R.id.registerTextView) TextView mRegisterTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
