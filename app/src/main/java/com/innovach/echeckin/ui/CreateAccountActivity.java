e57191e39edd7201c8e77011b7d0e79e3e0d5af3
viv-iam <vivian.opondoh@gmail.com>
2017-06-13 11:03:38 +0300

    Add ProgressDialog object to CreateAccountActivity *Add new member variable for ProgressDialog object *Call createAuthProgressDialog() method in the onCreate() method *Inside createAuthProgressDialog() set title and message values and setCancelable() to false *Show Progress Dialog in createNewUser() method by adding mAuthProgressDialog.show() *Dismiss the dialogue by calling mAuthProgressDialog.dismiss() in the createUserWithEmailPassword()

--- app/src/main/java/com/innovach/echeckin/ui/CreateAccountActivity.java
+++ app/src/main/java/com/innovach/echeckin/ui/CreateAccountActivity.java
@@ -1,5 +1,6 @@
 package com.innovach.echeckin.ui;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -30,8 +31,11 @@ public class CreateAccountActivity extends AppCompatActivity implements View.OnC
     @Bind(R.id.passwordEditText) EditText mPasswordEditText;
     @Bind(R.id.confirmPasswordEditText) EditText mConfirmPasswordEditText;
     @Bind(R.id.loginTextView) TextView mLoginTextView;
+
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
+    private ProgressDialog mAuthProgressDialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,7 +46,15 @@ public class CreateAccountActivity extends AppCompatActivity implements View.OnC
         mLoginTextView.setOnClickListener(this);
         mCreateUserButton.setOnClickListener(this);
         createAuthStateListener();
+        createAuthProgressDialog();
     }
+    private void createAuthProgressDialog() {
+        mAuthProgressDialog = new ProgressDialog(this);
+        mAuthProgressDialog.setTitle("Loading...");
+        mAuthProgressDialog.setMessage("Authenticating with Firebase...");
+        mAuthProgressDialog.setCancelable(false);
+    }
+
     @Override
     public void onClick(View view) {
         if (view == mLoginTextView) {
@@ -65,11 +77,14 @@ public class CreateAccountActivity extends AppCompatActivity implements View.OnC
         boolean validName = isValidName(name);
         boolean validPassword = isValidPassword(password, confirmPassword);
         if (!validEmail || !validName || !validPassword) return;
+        mAuthProgressDialog.show();
 
         mAuth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
+                        mAuthProgressDialog.dismiss();
+
                         if (task.isSuccessful()) {
                             Log.d(TAG, "Authentication successful");
                         } else {
