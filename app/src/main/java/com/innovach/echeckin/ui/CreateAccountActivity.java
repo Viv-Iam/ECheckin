e78242d9b8183acf11aefb7cf04d1ef400dea2bc
viv-iam <vivian.opondoh@gmail.com>
2017-06-13 03:50:56 +0300

    Add AuthStateListener to FirebaseAuth object *Associate AuthStateListener and FirebaseAuth by calling addAuthStateListener() in the onStart() *Remove listener by calling removeAuthStateListener() in the onStop()

--- app/src/main/java/com/innovach/echeckin/ui/CreateAccountActivity.java
+++ app/src/main/java/com/innovach/echeckin/ui/CreateAccountActivity.java
@@ -15,6 +15,7 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.innovach.echeckin.R;
 
 import butterknife.Bind;
@@ -72,5 +73,33 @@ public class CreateAccountActivity extends AppCompatActivity implements View.OnC
                         }
                     }
                 });
+    }
+    private void createAuthStateListener() {
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+    @Override
+    public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+        final FirebaseUser user = firebaseAuth.getCurrentUser();
+        if (user != null) {
+            Intent intent = new Intent(CreateAccountActivity.this, MainActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+            finish();
+        }
+    }
+
+};
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
     }
 }
